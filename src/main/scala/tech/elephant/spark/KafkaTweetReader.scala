2693eb0ec81d43ae0e1a3b867de3a3d461fc8f12
Oliver Meyn <oliver@mineallmeyn.com>
2016-03-28 16:08:21 -0400

    Add a naive hashtag trend follower

--- src/main/scala/tech/elephant/spark/KafkaTweetReader.scala
+++ src/main/scala/tech/elephant/spark/KafkaTweetReader.scala
@@ -16,17 +16,28 @@ import org.apache.spark.streaming.api.java.JavaStreamingContext
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.streaming.kafka._
 
+import scala.collection.immutable.ListMap
+import scala.util.matching.Regex
+
 /**
   * In Spark, reads Avro-encoded tweets from Kafka that were produced by the kafka-esque project, using the Confluent
-  * Schema Registry. Doesn't do anything interesting with the message - just dumps to log.
+  * Schema Registry. Maintains a naive count of popular hashtags and logs them every few seconds.
+  *
+  * NOTE: This is just me playing - there are surely better ways to do this both in terms of Scala, Spark, and Kafka!
   */
 object KafkaTweetReader {
+
+  val logger = Logger[this.type]
+
+  case class Tweet(id: Long, username: String, status: String)
+  var trending = scala.collection.mutable.Map[String, Int]()
+
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Streaming Avro msgs from Kafka")
     val sc = new SparkContext(conf)
     val sqlContext = new SQLContext(sc)
 
-    val ssc = new StreamingContext(sc, Seconds(2))
+    val ssc = new StreamingContext(sc, Seconds(10))
     val kafkaParams = Map[String, String](
       "metadata.broker.list" -> "blackcube.home:9092",
       "group.id" -> "test",
@@ -35,12 +46,34 @@ object KafkaTweetReader {
 
     val messages = KafkaUtils.createDirectStream[Object, Object, KafkaAvroDecoder, KafkaAvroDecoder](ssc, kafkaParams, topicSet)
 
-    // Just dump a message to prove we can read it
-    val lines = messages.map(msg => {
-      val (k, v) = msg
-      v.toString()
-    })
-    lines.print()
+    messages.foreachRDD { rdd =>
+      rdd.foreach { rawRecord =>
+        val record = rawRecord._2.asInstanceOf[GenericRecord]
+        val tweet = Tweet(record.get("id").asInstanceOf[Long], record.get("username").toString, record.get("status").toString)
+        val pattern = new Regex("\\B#\\w\\w+")
+        val hashtags = pattern findAllIn tweet.status
+        hashtags.foreach( ht => {
+          val rawCount = trending.get(ht)
+          val count = if (rawCount.isDefined) rawCount.get.toInt else 0
+          if (count == 0) {
+            trending.put(ht, 1)
+          } else {
+            trending.put(ht, count+1)
+          }
+        })
+
+        val sortedTrending = ListMap(trending.toSeq.sortWith(_._2 > _._2): _*).take(10)
+        logger.info("TRENDING:")
+        sortedTrending.foreach(t => {
+          val (ht, htcount) = t
+          logger.info(ht + " " + htcount.toString)
+        })
+      }
+
+      // keep the ht map from growing unbounded
+      val sortedTrending = ListMap(trending.toSeq.sortWith(_._2 > _._2): _*).take(100)
+      trending = scala.collection.mutable.Map(sortedTrending.toSeq: _*)
+    }
 
     // Start the computation
     ssc.start()
